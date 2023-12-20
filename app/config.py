3d451d1fda683cb10de68c180b1aae66fba75a43
hitol <1126031033@qq.com>
2023-12-20 13:05:38 +0800

    cache

--- app/config.py
+++ app/config.py
@@ -7,13 +7,15 @@ load_dotenv()
 DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URI")
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True)
 redis_client = redis.Redis(connection_pool=pool)
 
 SUMMARY_PROGRESS_KEY = "summary_progress"
+
 
 class Config:
     SECRET_KEY = os.environ.get('KEY') or '123456'
